6153150e8c8c5c11ef33a41d8f5236fe14af8258
Thomas Salah <thomas.salah@gmail.com>
2020-03-07 10:45:25 +0000

    Feature: adding toggleing between answered unanswered on dashoboard'

--- src/components/QuestionsDashboard.js
+++ src/components/QuestionsDashboard.js
@@ -1,54 +1,96 @@
-import React, { Component} from 'react'
-import { connect } from  'react-redux'
-import { handleReceiveQuestions } from '../actions/questions'
-import QuestionCard from './QuestionCard'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleReceiveQuestions } from "../actions/questions";
+import QuestionCard from "./QuestionCard";
 
 class QuestionsDashboard extends Component {
+  state = {
+    active: "Unanswered"
+  };
 
-  componentDidMount () {
-    const { dispatch } = this.props
+  componentDidMount() {
+    const { dispatch } = this.props;
 
-    dispatch(handleReceiveQuestions())
+    dispatch(handleReceiveQuestions());
   }
+
+  dashboardNavigate = e => {
+    return this.setState({ active: e.target.id });
+  };
 
   render() {
-    console.log('Dashboard props', this.props.user)
-    const { questionsId, user } = this.props
+    console.log("Dashboard props", this.props.user);
+    const { questionsId, user } = this.props;
+    const { active } = this.state;
 
-    const userQuestionsAnswered = user && Object.keys(user.answers)
-    let answeredQuestions
-    let unAnsweredQuestions
+    const userQuestionsAnswered = user && Object.keys(user.answers);
+    let answeredQuestions;
+    let unAnsweredQuestions;
 
-    if(userQuestionsAnswered && questionsId) {
-       answeredQuestions =  questionsId.filter(x => userQuestionsAnswered.includes(x))
-       unAnsweredQuestions =  questionsId.filter(x => !userQuestionsAnswered.includes(x))
+    if (userQuestionsAnswered && questionsId) {
+      answeredQuestions = questionsId.filter(x =>
+        userQuestionsAnswered.includes(x)
+      );
+      unAnsweredQuestions = questionsId.filter(
+        x => !userQuestionsAnswered.includes(x)
+      );
     }
 
-    return(
+    return (
       <div>
         <div>
-          <h2>answered questions</h2>
-          <ul>
-            {answeredQuestions && answeredQuestions.map(id => <li key={id}><QuestionCard id={id}/></li>)}
-          </ul>
+          <div onClick={e => this.dashboardNavigate(e)}>
+            <ul>
+              <li
+                id="Unanswered"
+                key="Unanswered"
+                className={active === "Unanswered" ? "active" : ""}
+              >
+                Unanswered questions
+              </li>
+              <li
+                id="Answered"
+                key="Answered"
+                className={active === "Answered" ? "active" : ""}
+              >
+                Answered questions
+              </li>
+            </ul>
+          </div>
+          <div>
+            {active === "Unanswered" ? (
+              <ul>
+                {unAnsweredQuestions &&
+                  unAnsweredQuestions.map(id => (
+                    <li key={id}>
+                      <QuestionCard id={id} />
+                    </li>
+                  ))}
+              </ul>
+            ) : (
+              <ul>
+                {answeredQuestions &&
+                  answeredQuestions.map(id => (
+                    <li key={id}>
+                      <QuestionCard id={id} />
+                    </li>
+                  ))}
+              </ul>
+            )}
+          </div>
         </div>
-        <div>
-          <h3>answered questions</h3>
-          <ul>
-            {unAnsweredQuestions && unAnsweredQuestions.map(id => <li key={id}><QuestionCard id={id}/></li>)}
-          </ul>
-        </div>
+        <div />
       </div>
-    )
+    );
   }
 }
 
-function mapStateToProps({questions, users, loggedInUser}) {
-  return{
+function mapStateToProps({ questions, users, loggedInUser }) {
+  return {
     questionsId: Object.keys(questions),
     questions,
-    user: users['sarahedo'],
-  }
+    user: users["sarahedo"]
+  };
 }
 
-export default connect(mapStateToProps)(QuestionsDashboard)
+export default connect(mapStateToProps)(QuestionsDashboard);
